f517ef7fd71841a8e1b3d41de90cccae97d0a980
keynaka <nakasone.nc@gmail.com>
2023-07-29 21:29:35 -0300

    small fixes

--- src/main/java/com/rkmd/toki_no_nagare/controller/ContactController.java
+++ src/main/java/com/rkmd/toki_no_nagare/controller/ContactController.java
@@ -17,16 +17,16 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @GetMapping("/{contact_id}")
-    public ResponseEntity<Contact> getContact(@PathVariable("contact_id") Long id) {
-        Optional<Contact> contact = contactService.get(id);
+    @GetMapping("/{dni}")
+    public ResponseEntity<Contact> getContact(@PathVariable("dni") Long dni) {
+        Optional<Contact> contact = contactService.get(dni);
         if (!contact.isPresent())
            return ResponseEntity.notFound().build();
         return ResponseEntity.ok().body(contact.get());
     }
 
     @PostMapping("")
-    public ResponseEntity<Contact> save(@RequestBody @Valid Map<String, Object> json) {
+    public ResponseEntity<Contact> createContact(@RequestBody @Valid Map<String, Object> json) {
         ValidationUtils.checkParam(json.containsKey("name"), "name_missing", "Name is missing and is mandatory");
         ValidationUtils.checkParam(json.containsKey("last_name"), "last_name_missing", "Last Name is missing and is mandatory");
         ValidationUtils.checkParam(json.containsKey("dni"), "dni_missing", "Dni is missing and is mandatory");
--- src/main/java/com/rkmd/toki_no_nagare/entities/booking/Booking.java
+++ src/main/java/com/rkmd/toki_no_nagare/entities/booking/Booking.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.rkmd.toki_no_nagare.entities.contact.Contact;
 import jakarta.persistence.*;
 
+import java.time.ZonedDateTime;
+
 @Entity
 @Table(name="booking")
 public class Booking {
@@ -28,4 +30,13 @@ public class Booking {
     @Enumerated(EnumType.STRING)
     @Column(name = "payment_method", nullable = false)
     private BookingPaymentMethod paymentMethod;
+
+    @Column(name = "date_created", nullable = false)
+    private ZonedDateTime dateCreated;
+
+    @Column(name = "last_updated", nullable = false)
+    private ZonedDateTime lastUpdated;
+
+    @Column(name = "expiration_date", nullable = false)
+    private ZonedDateTime expirationDate;
 }
