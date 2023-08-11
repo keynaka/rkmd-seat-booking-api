bb8f5cb63723901f77dafb7e11f5e7768031ff0d
keynaka <nakasone.nc@gmail.com>
2023-08-10 22:44:50 -0300

    removed: Unused SeatStatus

--- src/main/java/com/rkmd/toki_no_nagare/entities/seat/SeatStatus.java
+++ src/main/java/com/rkmd/toki_no_nagare/entities/seat/SeatStatus.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public enum SeatStatus {
     @JsonProperty("vacant")
     VACANT,
-    @JsonProperty("pre_reserved")
-    PRE_RESERVED, // Status when the seat was selected by te user to reserve it
     @JsonProperty("reserved")
     RESERVED,
     @JsonProperty("occupied")
