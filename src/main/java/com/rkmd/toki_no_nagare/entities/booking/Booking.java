fff129233571dff3df437b2ce9566837ad7ceb28
AleOshiro <alejandro.oshiro.12@gmail.com>
2023-08-16 23:36:01 -0300

    refactor: agrego la relación de Payment con Booking
    refactor: Se agrega la relación de la entidad Booking con la entidad Payment

--- src/main/java/com/rkmd/toki_no_nagare/entities/booking/Booking.java
+++ src/main/java/com/rkmd/toki_no_nagare/entities/booking/Booking.java
@@ -3,6 +3,7 @@ package com.rkmd.toki_no_nagare.entities.booking;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.rkmd.toki_no_nagare.entities.contact.Contact;
+import com.rkmd.toki_no_nagare.entities.payment.Payment;
 import com.rkmd.toki_no_nagare.entities.seat.Seat;
 import jakarta.persistence.*;
 
@@ -30,10 +31,6 @@ public class Booking {
     @Column(name = "status", nullable = false)
     private BookingStatus status;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "payment_method", nullable = false)
-    private BookingPaymentMethod paymentMethod;
-
     @Column(name = "date_created", nullable = false)
     private ZonedDateTime dateCreated;
 
@@ -42,6 +39,10 @@ public class Booking {
 
     @Column(name = "expiration_date", nullable = false)
     private ZonedDateTime expirationDate;
+
+    @OneToOne
+    @JoinColumn(name = "paymentId")
+    private Payment payment;
 
     @JsonManagedReference
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "booking", cascade = CascadeType.ALL)
@@ -79,14 +80,6 @@ public class Booking {
         this.status = status;
     }
 
-    public BookingPaymentMethod getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(BookingPaymentMethod paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
     public ZonedDateTime getDateCreated() {
         return dateCreated;
     }
@@ -117,5 +110,13 @@ public class Booking {
 
     public void setSeats(List<Seat> seats) {
         this.seats = seats;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }
