646bb82640fc336bdc18241e7913affdec3a124e
AleOshiro <alejandro.oshiro.12@gmail.com>
2023-08-16 23:34:37 -0300

    feat: agrego nueva entidad Payment y enums de estados
    feat: Se agrega nueva entidad Payment y enums donde se define "la forma de pago" y "el estado del pago"

---
+++ src/main/java/com/rkmd/toki_no_nagare/entities/payment/Payment.java
@@ -0,0 +1,60 @@
+package com.rkmd.toki_no_nagare.entities.payment;
+
+import com.rkmd.toki_no_nagare.entities.booking.Booking;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Entity
+@SequenceGenerator(name = "seq_payment", initialValue = 1, allocationSize = 1, sequenceName = "seq_payment")
+@Table(name = "payment")
+@Getter
+public class Payment {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_payment")
+  private Long paymentId;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private PaymentStatus paymentStatus;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private PaymentMethod paymentMethod;
+
+  @NotNull
+  private BigDecimal amount;
+
+  @Setter
+  private String receiptNumber;
+
+  @NotNull
+  private ZonedDateTime dateCreated;
+
+  @Setter
+  private ZonedDateTime lastUpdated;
+
+  @NotNull
+  private ZonedDateTime expirationDate;
+
+  @Version
+  private int version;
+
+  @OneToOne
+  @JoinColumn(name = "paymentId")
+  private Booking booking;
+
+  public Payment(PaymentMethod paymentMethod, BigDecimal amount, ZonedDateTime dateCreated, ZonedDateTime expirationDate){
+    this.paymentStatus = PaymentStatus.PENDING;
+    this.amount = amount;
+    this.paymentMethod = paymentMethod;
+    this.dateCreated = dateCreated;
+    this.expirationDate = expirationDate;
+  }
+
+}
