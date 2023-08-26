76920b311c5727dd30aeba985bf3943b82680ae7
KOMESU, PABLO <pkomesu@nacionservicios.com.ar>
2023-08-26 15:36:15 -0300

    feat: endpoint para envío de e-mails
    Se agrega endpoint para envío de e-mails.

--- src/main/java/com/rkmd/toki_no_nagare/controller/ContactController.java
+++ src/main/java/com/rkmd/toki_no_nagare/controller/ContactController.java
@@ -1,7 +1,10 @@
 package com.rkmd.toki_no_nagare.controller;
 
 import com.rkmd.toki_no_nagare.entities.contact.Contact;
+import com.rkmd.toki_no_nagare.entities.payment.PaymentMethod;
 import com.rkmd.toki_no_nagare.service.ContactService;
+import com.rkmd.toki_no_nagare.service.mailing.IMailingService;
+import com.rkmd.toki_no_nagare.service.mailing.TransportMailSenderImpl;
 import com.rkmd.toki_no_nagare.utils.ValidationUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,9 @@ import java.util.Optional;
 public class ContactController {
     @Autowired
     private ContactService contactService;
+
+    @Autowired
+    private TransportMailSenderImpl transportMailSenderImpl;
 
     @GetMapping("/{dni}")
     public ResponseEntity<Contact> getContact(@PathVariable("dni") Long dni) {
@@ -38,5 +44,26 @@ public class ContactController {
         Contact newContact = contactService.createContact(json);
 
         return ResponseEntity.ok().body(newContact);
+    }
+
+    /** Uso provisorio. Eliminar este endpoint al finalizar los tests de envío de e-mails. */
+    @Deprecated
+    @GetMapping("/test/notifications/reservations/{email}/{paymentMethod}/{name}/{lastname}")
+    public ResponseEntity<String> testSendMail(@PathVariable("email") String email,
+                                               @PathVariable("paymentMethod") String paymentMethod,
+                                               @PathVariable("name") String name,
+                                               @PathVariable("lastname") String lastname){
+
+        PaymentMethod enumPaymentMethod;
+
+        if(paymentMethod.equals("mercado_pago")){
+            enumPaymentMethod = PaymentMethod.MERCADO_PAGO;
+        } else {
+            enumPaymentMethod = PaymentMethod.CASH;
+        }
+
+        IMailingService mailingService = transportMailSenderImpl;
+
+        return ResponseEntity.ok(mailingService.notifyReservation(email, name, lastname, "asdf1231", enumPaymentMethod));
     }
 }
