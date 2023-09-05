43b56b680b536c6dc6e9b2fb55e8ca9f3be15bbc
keynaka <nakasone.nc@gmail.com>
2023-09-05 18:25:34 -0300

    add composite-id equals and hashCode override

--- src/main/java/com/rkmd/toki_no_nagare/entities/admin_available_date/AdminAvailableDateId.java
+++ src/main/java/com/rkmd/toki_no_nagare/entities/admin_available_date/AdminAvailableDateId.java
@@ -2,6 +2,7 @@ package com.rkmd.toki_no_nagare.entities.admin_available_date;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
 public class AdminAvailableDateId implements Serializable {
     private ZonedDateTime initDate;
@@ -13,4 +14,18 @@ public class AdminAvailableDateId implements Serializable {
     }
 
     public AdminAvailableDateId() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AdminAvailableDateId that = (AdminAvailableDateId) o;
+        return Objects.equals(initDate, that.initDate) &&
+                Objects.equals(place, that.place);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(initDate, place);
+    }
 }
