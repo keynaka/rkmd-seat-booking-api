4f8e3ffaf999a1829777f5384c5a5a789bd9d33c
keynaka <nakasone.nc@gmail.com>
2023-09-06 21:15:58 -0300

    /admin/available-dates/{paymentMethod} implementation

--- src/main/java/com/rkmd/toki_no_nagare/controller/AdminAvailableDateController.java
+++ src/main/java/com/rkmd/toki_no_nagare/controller/AdminAvailableDateController.java
@@ -2,7 +2,12 @@ package com.rkmd.toki_no_nagare.controller;
 
 import com.rkmd.toki_no_nagare.dto.admin_available_date.CreateAdminAvailableDateRequestDto;
 import com.rkmd.toki_no_nagare.entities.admin_available_date.AdminAvailableDate;
+import com.rkmd.toki_no_nagare.entities.payment.PaymentMethod;
+import com.rkmd.toki_no_nagare.entities.seat.SeatSector;
+import com.rkmd.toki_no_nagare.exception.BadRequestException;
 import com.rkmd.toki_no_nagare.service.AdminAvailableDateService;
+import com.rkmd.toki_no_nagare.service.expiration.ExpirationService;
+import com.rkmd.toki_no_nagare.service.expiration.ExpirationServiceFactory;
 import com.rkmd.toki_no_nagare.utils.Tools;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +24,30 @@ public class AdminAvailableDateController {
     @Autowired
     private AdminAvailableDateService adminAvailableDateService;
 
+    @Autowired
+    private ExpirationServiceFactory expirationServiceFactory;
+
     @GetMapping("")
     public ResponseEntity<List<AdminAvailableDate>> getAvailableDates(@RequestParam(value = "expirationDate", required = false) String expirationDate) {
         ZonedDateTime expirationDateFormatted = expirationDate != null ? Tools.formatDateStringToZonedDateTime(expirationDate) : null;
         List<AdminAvailableDate> availableDates = adminAvailableDateService.getAvailableDates(expirationDateFormatted);
+        return ResponseEntity.ok().body(availableDates);
+    }
+
+    @GetMapping("/{paymentMethod}")
+    public ResponseEntity<List<AdminAvailableDate>> getAvailableDatesByPaymentMethod(@PathVariable("paymentMethod") String paymentMethodInput) {
+        PaymentMethod paymentMethod;
+        try {
+            paymentMethod = PaymentMethod.valueOf(paymentMethodInput.toUpperCase());
+        } catch (Exception e) {
+            throw new BadRequestException("Invalid_payment_method", "Invalid payment method");
+        }
+
+        ZonedDateTime expirationDate = expirationServiceFactory
+                .getExpirationService(paymentMethod)
+                .getExpirationDate(Tools.getCurrentDate());
+        List<AdminAvailableDate> availableDates = adminAvailableDateService.getAvailableDates(expirationDate);
+
         return ResponseEntity.ok().body(availableDates);
     }
 
