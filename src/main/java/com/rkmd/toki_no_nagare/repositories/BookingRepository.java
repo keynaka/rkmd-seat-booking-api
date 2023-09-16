252d85a9654d70ec8cb1d3da56ae63eeab2d4175
AleOshiro <alejandro.oshiro.12@gmail.com>
2023-09-15 21:42:41 -0300

    refactor: agrego busqueda por booking code
    refactor: Se agrega busqueda de booking por booking code y elimino envio de mails en BookingService

--- src/main/java/com/rkmd/toki_no_nagare/repositories/BookingRepository.java
+++ src/main/java/com/rkmd/toki_no_nagare/repositories/BookingRepository.java
@@ -4,7 +4,11 @@ import com.rkmd.toki_no_nagare.entities.booking.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findAllByOrderByDateCreatedDesc();
+
+    Optional<Booking> findByHashedBookingCode(String bookingCode);
+
 }
