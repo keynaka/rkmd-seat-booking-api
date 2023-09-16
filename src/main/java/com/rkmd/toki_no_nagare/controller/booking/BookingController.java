e6bae76b04731197e46d8dbf73c97f153a1f696a
AleOshiro <alejandro.oshiro.12@gmail.com>
2023-09-16 00:50:16 -0300

    refactor: modifico el envio de mails de una reserva
    refactor: Se modifica el momento en el que se envia el mail (de reserva y backup) para evitar envios erróneos ante una condición de carrera en reservas de la misma butaca

--- src/main/java/com/rkmd/toki_no_nagare/controller/booking/BookingController.java
+++ src/main/java/com/rkmd/toki_no_nagare/controller/booking/BookingController.java
@@ -3,8 +3,11 @@ package com.rkmd.toki_no_nagare.controller.booking;
 import com.rkmd.toki_no_nagare.dto.booking.BookingResponseDto;
 import com.rkmd.toki_no_nagare.dto.booking.CreateBookingRequestDto;
 import com.rkmd.toki_no_nagare.dto.booking.CreateBookingResponseDto;
+import com.rkmd.toki_no_nagare.entities.booking.Booking;
 import com.rkmd.toki_no_nagare.entities.booking.BookingStatus;
 import com.rkmd.toki_no_nagare.service.BookingService;
+import com.rkmd.toki_no_nagare.service.mailing.AbstractMailingService;
+import com.rkmd.toki_no_nagare.utils.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +19,36 @@ public class BookingController implements BookingControllerResources{
     @Autowired
     private BookingService bookingService;
 
+    @Autowired
+    private AbstractMailingService mailingService;
+
     @PostMapping(value = "/v2/bookings", produces = "application/json")
     @ResponseStatus(value = HttpStatus.CREATED)
     public CreateBookingResponseDto createBooking(CreateBookingRequestDto request) {
-        return bookingService.createBooking(request);
+        CreateBookingResponseDto response = bookingService.createBooking(request);
+        Booking booking = bookingService.getBookingByBookingCode(response.getBookingCode());
+
+        if(booking != null){
+            // Step 9: Notify reservation by sending an e-mail to the client
+            mailingService.notifyReservation(
+                booking.getClient().getEmail(),
+                booking.getClient().getName(),
+                booking.getClient().getLastName(),
+                booking.getHashedBookingCode(),
+                booking.getPayment().getPaymentMethod(),
+                booking.getExpirationDate(),
+                Tools.convertSeatToSeatDto(booking.getSeats()));
+
+            // Step 10: Notify reservation by sending an e-mail to the backend for backup
+            mailingService.notifyReservationBackUp(
+                booking.getHashedBookingCode(),
+                booking.toString(),
+                booking.getClient().toString(),
+                booking.getPayment().toString(),
+                booking.getSeats().toString());
+        }
+
+        return response;
     }
 
     @GetMapping("/v3/bookings/{code_id}")
